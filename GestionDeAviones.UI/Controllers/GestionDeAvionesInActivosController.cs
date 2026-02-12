565f428f1564f11a6a0785b0b2c4bf72a9c9fded
Aurora <aurorabogantes@gmail.com>
2026-02-12 10:52:39 -0600

    Añadir controlador para gestión de aviones inactivos
    Se crea el controlador GestionDeAvionesInActivosController, que utiliza inyección de dependencias para acceder a ServicioApi. Se implementa la acción Index para obtener y mostrar la lista de aviones inactivos mediante el método ObtenerAvionesInActivosAsync.

---
+++ GestionDeAviones.UI/Controllers/GestionDeAvionesInActivosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestionDeAviones.UI.Controllers
+{
+    public class GestionDeAvionesInActivosController(ServicioApi servicioApi) : Controller)
+    {
+        readonly ServicioApi _servicioApi = servicioApi;
+        // GET: GestionDeAvionesInActivosController
+        public async Task<ActionResult> Index()
+        {
+            var listaDeInactivos = await _servicioApi.ObtenerAvionesInActivosAsync();
+            return View(listaDeInactivos);
+        }
+    }
+}
