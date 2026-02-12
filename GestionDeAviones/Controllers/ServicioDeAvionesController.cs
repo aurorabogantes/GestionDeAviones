120081effa159f0705d4b3921a6c990f431d3f2c
Aurora <aurorabogantes@gmail.com>
2026-02-11 20:44:12 -0600

    Añadir endpoints CRUD y de activación para aviones
    Se agregan nuevos endpoints al ServicioDeAvionesController para:
    - Listar aviones activos e inactivos
    - Obtener un avión por ID
    - Agregar y editar aviones
    - Activar y desactivar aviones
    
    Todos los métodos utilizan operaciones asíncronas a través de la capa de administración. Nota: el endpoint de inactivos llama al método de activos, lo que podría requerir revisión.

--- GestionDeAviones/Controllers/ServicioDeAvionesController.cs
+++ GestionDeAviones/Controllers/ServicioDeAvionesController.cs
@@ -1,6 +1,7 @@
 ﻿using GestionDeAviones.BL;
 using GestionDeAviones.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 
 namespace GestionDeAviones.SI.Controllers
 {
@@ -20,6 +21,57 @@ namespace GestionDeAviones.SI.Controllers
         {
             var lista = await _admin.ObtengaLaListaAsync();
             return Ok(lista);
+        }
+
+        [HttpGet("ObtengaLaListaDeActivos")]
+        public async Task<ActionResult<IEnumerable<Avion>>> ObtengaLaListaDeActivos()
+        {
+            var lista = await _admin.ObtengaLaListaDeActivosAsync();
+            return Ok(lista);
+        }
+
+        [HttpGet("ObtengaLaListaDeInActivos")]
+        public async Task<ActionResult<IEnumerable<Avion>>> ObtengaLaListaDeInActivos()
+        {
+            var lista = await _admin.ObtengaLaListaDeActivosAsync();
+            return Ok(lista);
+        }
+
+        [HttpGet("ObtengaElAvion")]
+        public async Task<ActionResult<Avion>> ObtengaElAvion(int id)
+        {
+            var avion = await _admin.ObtengaElAvionAsync(id);
+            if (avion == null)
+                return NotFound();
+            return Ok(avion);
+        }
+
+        [HttpPost("Agregue")]
+        public async Task<IActionResult> Agregue([FromBody] Avion avion)
+        {
+            await _admin.AgregueAsync(avion);
+            return Ok();
+        }
+
+        [HttpPut("EditeElAvion")]
+        public async Task<IActionResult> EditeElAvion([FromBody] Avion avion)
+        {
+            await _admin.EditeElAvionAsync(avion);
+            return Ok();
+        }
+
+        [HttpPut("Active")]
+        public async Task<IActionResult> Active(int id)
+        {
+            await _admin.ActiveAsync(id);
+            return Ok();
+        }
+
+        [HttpPut("DesActive")]
+        public async Task<IActionResult> DesActive(int id)
+        {
+            await _admin.DesActiveAsync(id);
+            return Ok();
         }
     }
 }
